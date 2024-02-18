b9f77d33e60fb24fb408480d34dec6c27156d7e7
vishal.sr@hotmail.com <vishal.sr@hotmail.com>
2024-02-18 18:35:58 +0530

    Added few API requests

--- Project 2/books2.py
+++ Project 2/books2.py
@@ -12,6 +12,16 @@ class Book(BaseModel):
     description: str = Field(min_length=1, max_length=250)
     rating: int = Field(gt=0, lt=6)
 
+    class Config:
+        json_schema_extra = {
+            "example": {
+                "title": "New book",
+                "author": "New Author",
+                "description": "New book's description",
+                "rating": 3
+            }
+        }
+
 
 books = [
     Book(id=1, title="Book 1", author="Author 1",
@@ -42,11 +52,38 @@ async def get_all_books():
     return books
 
 
-@app.post("/create-book")
+@app.post("/create-book/")
 async def create_book(book: Book):
     book = Book(**book.model_dump())
     book.id = next_book_id()
     books.append(book)
+
+
+@app.get("/book/{bookID}/")
+async def get_book_by_id(bookID: int) -> Book:
+    for book in books:
+        if book.id == bookID:
+            return book
+
+
+@app.get("/books/")
+async def filter_books_by_rating(rating: int) -> list[Book]:
+    return [book for book in books if book.rating == rating]
+
+
+@app.put("/book/update-book/")
+async def update_book(book: Book):
+    for i, bk in enumerate(books):
+        if bk.id == book.id:
+            books[i] = book
+
+
+@app.delete("/book/delete")
+async def delete_book_by_id(id: int):
+    for i, book in enumerate(books):
+        if book.id == id:
+            books.pop(i)
+            break
 
 
 def next_book_id() -> int: return books[-1].id + 1 if len(books) > 0 else 1
